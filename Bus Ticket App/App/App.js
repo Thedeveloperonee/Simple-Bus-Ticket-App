54816f1f2c69937f09814614be9921a6743a8fcf
Thedeveloperonee <Balyzaa1905@proton.me>
2026-01-31 21:23:43 +0300

    Update App.js

---
+++ Bus Ticket App/App/App.js
@@ -0,0 +1,19 @@
+const app=document.querySelector(".app");
+const signincontainer=document.querySelector(".signincontainer");
+const Homepage=document.querySelector(".Homepage");
+Homepage.remove();
+const ticketprintpage=document.querySelector(".ticketprintpage");
+ticketprintpage.remove();
+const Signinbuttonn=document.querySelector(".Signinbuttonn");
+
+
+
+
+
+
+
+
+
+
+
+
